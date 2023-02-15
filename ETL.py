08419bf05b3154378a4c1cd8feb3f7f374a5fc0e
Théo Gaschard <theo.gaschard@ynov.com>
2023-02-15 14:49:27 +0100

    Ajout Adresse et CP + Filtrage "ecole" et doublons

--- ETL.py
+++ ETL.py
@@ -10,24 +10,35 @@ import MapCategory as CategoryMap
 import MapFiltreLyon as FiltreLyonMap
 
 # Création du DataFrame
-dataframe = pd.DataFrame(columns=['Nom', 'Commune', 'Type','Coord_X', 'Coord_Y'])
+dataframe = pd.DataFrame(columns=['Nom', 'Commune','Adresse','Code postal','Type','Coord_X', 'Coord_Y'])
 
 # Requete à l'API du gouvernement
-response = rq.get("https://equipements.sports.gouv.fr/api/records/1.0/search/?dataset=data-es&q=&refine.code_dept=69&rows=10000")
+response = rq.get("https://equipements.sports.gouv.fr/api/records/1.0/search/?dataset=data-es&q=&refine.code_dept=69&rows=10000&facet=adresse")
 data = response.json()
 for record in data['records']:
   rc = record['fields']
-  dataframe.loc[len(dataframe.index)] = [rc['nominstallation'],rc['nom_commune'],rc['typequipement'],rc['coordgpsx'],rc['coordgpsy']]
+  if 'adresse' in rc:
+    record_adresse = rc['adresse']
+  else:
+    record_adresse = str(rc['coordgpsy']) + '° N, ' + str(rc['coordgpsx']) + '° E'
+  dataframe.loc[len(dataframe.index)] = [rc['nominstallation'],rc['nom_commune'],record_adresse,rc['codepostal'],rc['typequipement'],rc['coordgpsx'],rc['coordgpsy']]
 
 # Requete à l'API du Grand Lyon
 response = rq.get("https://download.data.grandlyon.com/ws/rdata/urbalyon.recenseqptsport/all.json?maxfeatures=10000&start=1")
 data = response.json()
 for record in data['values']: 
-  dataframe.loc[len(dataframe.index)] = [record['nom'],record['commune'],record['type'],record['lon'],record['lat']]
+  if 'InsAdresse' in record['installation_details']:
+    record_adresse = record['installation_details']['InsAdresse']
+  else:
+    record_adresse = str(record['lat']) + '° N, ' + str(record['lon']) + '° E'
+  dataframe.loc[len(dataframe.index)] = [record['nom'],record['commune'],record_adresse,record['installation_details']['InsCodePostal'],record['type'],record['lon'],record['lat']]
 
 # Traitement des données
 ## Suppression des doublons
-dataframe = dataframe.drop_duplicates() # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop_duplicates.html
+  # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop_duplicates.html
+dataframe.drop_duplicates(inplace=True)
+dataframe.drop_duplicates(subset=['Type','Coord_X','Coord_Y'],inplace=True) # Peut supprimer des entrées en trop dans la Category "Autres"
+dataframe.drop_duplicates(subset=['Nom','Adresse','Code postal','Type'],inplace=True)
 
 ## Filtre sur Lyon
 # Ajout d'une colonne "Commune = Lyon" avec des valeurs booléens  : True si la valeur de Commune est Lyon ef Nan sinon
@@ -39,7 +50,7 @@ dataframe = dataframe.drop(columns='IsLyon')
 
 ## Filtre sur les lieux publics
 # On supprime les entrées contenant "college", "lycee", ...
-dataframe = dataframe.loc[~dataframe['Nom'].str.contains(r'(college|collège|lycée|lycee)', case=False, na=False)]
+dataframe = dataframe.loc[~dataframe['Nom'].str.contains(r'(college|collège|lycée|lycee|ecole)', case=False, na=False)]
 
 ## Catégorisation des sports
 category_map = CategoryMap.categories
@@ -63,6 +74,8 @@ for index, row in dataframe.iterrows():
     "properties": {
       "Name": row["Nom"],
       "Commune" : row["Commune"],
+      "Adresse" : row["Adresse"],
+      "Code Postal" : row["Code postal"],
       "Type" : row["Type"],
       "Category" : row["Category"]
     },
@@ -80,5 +93,5 @@ for index, row in dataframe.iterrows():
 GeoJson = json.dumps(GeoJson)
 
 ## Création du fichier "GeoActivities.json"
-with open("GeoActivities.geojson", "w") as outfile:
+with open("Activities.geojson", "w") as outfile:
   outfile.write(GeoJson)
